8463d953196e7877559f2d17bba373cdf25c7b50
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-10-13 03:13:03 +0600

    api done

--- go.mod
+++ go.mod
@@ -2,13 +2,18 @@ module go-speedtest
 
 go 1.18
 
-require github.com/showwin/speedtest-go v1.1.5
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+	github.com/kaimu/speedtest v0.0.0-20220111074741-408ab75d3ca1
+	github.com/rs/cors v1.8.2
+	github.com/showwin/speedtest-go v1.1.5
+)
 
 require (
 	github.com/ddo/go-fast v0.0.0-20190807090025-2ab4a3dd618c // indirect
 	github.com/ddo/pick-json v0.0.0-20170207095303-c8760e09e0fe // indirect
 	github.com/ddo/rq v0.0.0-20190828174524-b3daa55fcaba // indirect
-	github.com/kaimu/speedtest v0.0.0-20220111074741-408ab75d3ca1 // indirect
 	golang.org/x/net v0.0.0-20220107192237-5cfca573fb4d // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	gopkg.in/ddo/go-dlog.v2 v2.1.0 // indirect
--- main.go
+++ main.go
@@ -2,17 +2,56 @@ package main
 
 import (
 	"fmt"
-	"github.com/showwin/speedtest-go/speedtest"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/kaimu/speedtest/providers/netflix"
+	"github.com/rs/cors"
+	"github.com/showwin/speedtest-go/speedtest"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
+	// Load .env file to use the environment variable
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	r := mux.NewRouter()
+	p := os.Getenv("PORT")
+
+	r.HandleFunc("/",SpeedTester).Methods("GET")
+
+	c := cors.New(cors.Options{
+
+		// AllowCredentials: true,
+		AllowedMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "DELETE"},
+		AllowedOrigins:     []string{"*"},
+		AllowedHeaders:     []string{"Content-Type", "content-type", "Origin", "Accept", "Access-Control-Allow-Origin"},
+		OptionsPassthrough: false,
+		// Enable Debugging for testing, consider disabling in production
+		// Debug: true,
+	})
+	handler := c.Handler(r)
+	fmt.Println("Server is running on port: ", strings.Split(":"+p, ":")[1])
+	log.Fatal(http.ListenAndServe(":"+p, handler))
+}
+
+func Netflix() (up float64, err error) {
+	return netflix.Fetch()
+}
+
+func SpeedTester(w http.ResponseWriter, r *http.Request) {
 	user, err := speedtest.FetchUserInfo()
 
 	serverList, err := speedtest.FetchServers(user)
 	targets, err := serverList.FindServer([]int{})
 
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
 
@@ -24,14 +63,10 @@ func main() {
 		fmt.Printf("Speed (SpeedTest): Latency: %s, Download: %f, Upload: %f\n", s.Latency, s.DLSpeed, s.ULSpeed)
 	}
 
-	res,err := Netflix()
+	res, err := Netflix()
 	fmt.Print("Speed (Fast): ")
-	if(err != nil){
+	if err != nil {
 		fmt.Print(err)
 	}
 	fmt.Print(res)
-}
-
-func Netflix() (up float64, err error) {
-	return netflix.Fetch()
 }
